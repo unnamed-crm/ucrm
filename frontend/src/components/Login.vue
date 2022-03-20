fbb6485a1e88168ad60ab5c3efe06d19a1c42be5
Andrey Klyuev <46931423+LeenDrew@users.noreply.github.com>
2022-03-20 20:28:39 +0600

    refactor: refactored and linted (#58)

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,27 +1,33 @@
 <template>
- <div>
-   <form class="login" @submit.prevent="login">
-     <h1>Sign in</h1>
-     <label>Email</label>
-     <input required v-model="email" type="email" placeholder="Email"/>
-     <label>Password</label>
-     <input required v-model="password" type="password" placeholder="Password"/>
-     <hr/>
-     <button type="submit">Login</button>
-   </form>
- </div>
+  <div>
+    <form class="login" @submit.prevent="login">
+      <h1>Sign in</h1>
+      <label>Email</label>
+      <input required v-model="email" type="email" placeholder="Email" />
+      <label>Password</label>
+      <input
+        required
+        v-model="password"
+        type="password"
+        placeholder="Password"
+      />
+      <hr />
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-  export default {
-    methods: {
-      login: function() {
-        let email = this.email 
-        let password = this.password
-        this.$store.dispatch('login', { email, password })
-       .then(() => this.$router.push('/'))
-       .catch(err => console.log(err))
-      }
-    }
-  }
+export default {
+  methods: {
+    login() {
+      let email = this.email;
+      let password = this.password;
+      this.$store
+        .dispatch("login", { email, password })
+        .then(() => this.$router.push("/"))
+        .catch((err) => console.log(err));
+    },
+  },
+};
 </script>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,64 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <span><router-link to="/">Home</router-link></span>
+      <span><router-link to="/about">About</router-link></span>
+      <span v-if="isLoggedIn"><a @click="logout">Logout</a></span>
+      <span v-if="!isLoggedIn"><a @click="login">Login</a></span>
+      <span v-if="!isLoggedIn"><a @click="register">Register</a></span>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/login");
+    },
+    login() {
+      this.$router.push("/login");
+    },
+    register() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
+
+<style>
+a {
+  list-style: none;
+}
+
+.nav > span {
+  font-weight: bold;
+  color: #2c3e50;
+  position: relative;
+  padding: 0 10px;
+}
+
+.nav > *:not(:last-child)::after {
+  content: "";
+  background-color: #000;
+  position: absolute;
+  width: 1px;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+
+a:hover {
+  cursor: pointer;
+}
+
+a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
