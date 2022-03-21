abac0336af4e5bf1a20faf227742c34696003607
IndoorGhoul <hermione1granger121@gmail.com>
2022-03-21 15:32:38 +0600

    chore: fixed names

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
-import Login from "../components/Login.vue";
-import Register from "../components/Register.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 import store from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: Home,
     meta: {
       requiresAuth: true,
@@ -15,12 +15,12 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/login",
-    name: "login",
+    name: "Login",
     component: Login,
   },
   {
     path: "/register",
-    name: "register",
+    name: "Register",
     component: Register,
   },
 ];
