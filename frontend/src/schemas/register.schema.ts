d978df92baac424f4cdeaca070271dc5891cb22c
IndoorGhoul <hermione1granger121@gmail.com>
2022-03-27 15:06:11 +0600

    feat: form schemas

---
+++ frontend/src/schemas/register.schema.ts
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+export const registerSchema = yup.object({
+  email: yup
+    .string()
+    .required("This field is required")
+    .email("Email is not valid"),
+  password: yup.string().required("This field is required"),
+  confirmPassword: yup
+    .string()
+    .required("This field is required")
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+  // verificationCode: yup
+  //   .string()
+  //   .notRequired()
+  //   .length(5, "Verification Code are not full"),
+});
+
+export type RegisterSchema = typeof registerSchema;
+export type RegisterData = yup.InferType<typeof registerSchema>;
