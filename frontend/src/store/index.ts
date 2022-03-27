d445e677927ce31e03ee97f4904eaf3011a3dbe7
ignavan39 <ivanignatenko28@gmail.com>
2022-03-27 10:28:31 +0300

    fix store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,9 @@
-
 import { createStore } from "vuex";
 import { authModule } from "./modules/auth";
+import { RootState } from "./types";
 
-export const HOST_URL = process.env.host || 'http://localhost:8081/api/v1'
+export const HOST_URL = process.env.host || "http://localhost:8081/api/v1";
 
-export default createStore({
-  modules: {auth: authModule},
+export default createStore<RootState>({
+  modules: { auth: authModule },
 });
--- frontend/src/store/modules/auth/index.ts
+++ frontend/src/store/modules/auth/index.ts
@@ -1,9 +1,11 @@
+import { Module } from "vuex";
+import { RootState } from "../../types";
 import { actions } from "./actions";
 import { getters } from "./getters";
 import { mutations } from "./mutations";
-import { state } from "./state";
+import { State, state } from "./state";
 
-export const authModule = {
+export const authModule: Module<State,RootState> = {
   state,
   mutations,
   actions,
