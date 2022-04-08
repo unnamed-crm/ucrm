502172d5377cae1505ba9c37c0b31215f32b4759
Andrey Klyuev <46931423+LeenDrew@users.noreply.github.com>
2022-04-08 17:40:44 +0600

    [FEAT] frontend verification code (#95)
    * feat: verification code
    
    * feat: pass data from child to parent
    
    * feat: complete verification code
    
    * chore: linted
    
    * chore: add constant
    
    * feat: resend timer

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,2 @@
+export const VERIFICATION_CODE_LENGTH = 5;
+export const VERIFICATION_CODE_RESEND_TIME = 5 * 60;
---
+++ frontend/src/hooks/useTimer.ts
@@ -0,0 +1,56 @@
+import { reactive, computed, ComputedRef } from "vue";
+
+type Timer = {
+  startTime: number;
+  interval: number | null;
+};
+
+type Time = {
+  mins: ComputedRef<string>;
+  secs: ComputedRef<string>;
+};
+
+type useTimerReturnType = {
+  createInterval: () => void;
+  removeInterval: () => void;
+  time: Time;
+  isTimerLeft: ComputedRef<boolean>;
+};
+
+export const useTimer = (timeToLeft: number): useTimerReturnType => {
+  const timer = reactive<Timer>({
+    startTime: timeToLeft,
+    interval: null,
+  });
+  const mins = computed<string>(() => {
+    const value = Math.floor(timer.startTime / 60).toString();
+    if (value.length === 1) return `0${value}`;
+    return value;
+  });
+  const secs = computed<string>(() => {
+    const value = (timer.startTime - parseInt(mins.value, 10) * 60).toString();
+    if (value.length === 1) return `0${value}`;
+    return value;
+  });
+  const isTimerLeft = computed<boolean>(() => !timer.startTime);
+
+  const time: Time = {
+    mins,
+    secs,
+  };
+
+  const createInterval = () => {
+    timer.startTime = timeToLeft;
+    timer.interval = setInterval(() => {
+      timer.startTime -= 1;
+      if (!timer.startTime) removeInterval();
+    }, 1000) as unknown as number;
+  };
+
+  const removeInterval = () => {
+    clearInterval(timer.interval);
+    timer.interval = null;
+  };
+
+  return { createInterval, removeInterval, time, isTimerLeft };
+};
