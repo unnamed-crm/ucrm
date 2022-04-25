e8623c61cd0e476bee903c7988ca1b4e760982e5
ignavan39 <ivanignatenko28@gmail.com>
2022-04-25 20:04:04 +0300

    additional

--- sql/migrations/5_triggers.sql
+++ sql/migrations/5_triggers.sql
@@ -31,3 +31,54 @@ CREATE TRIGGER insert_custom_fields
     ON fields
     FOR EACH ROW
 EXECUTE PROCEDURE create_custom_fields();
+
+
+CREATE OR REPLACE FUNCTION create_card_fields()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+    arow record;
+    _id  uuid;
+BEGIN
+    FOR arow IN SELECT id from fields where type = 'card'
+        LOOP
+            _id := arow.id;
+            INSERT INTO card_fields (card_id, field_id) VALUES (new.id, _id);
+        END LOOP;
+
+    RETURN NEW;
+END
+$$;
+
+CREATE TRIGGER create_card_fields
+    AFTER INSERT
+    ON cards
+    FOR EACH ROW
+EXECUTE PROCEDURE create_card_fields();
+
+CREATE OR REPLACE FUNCTION create_contact_fields()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+    arow record;
+    _id  uuid;
+BEGIN
+    FOR arow IN SELECT id from fields where type = 'contact'
+        LOOP
+            _id := arow.id;
+            INSERT INTO contact_fields (contact_id, field_id) VALUES (new.id, _id);
+        END LOOP;
+
+    RETURN NEW;
+END
+$$;
+
+CREATE TRIGGER create_contact_fields
+    AFTER INSERT
+    ON contacts
+    FOR EACH ROW
+EXECUTE PROCEDURE create_contact_fields();
